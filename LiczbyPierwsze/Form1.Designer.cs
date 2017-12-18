0ef26e6edf0c2a223c3d860fb5aa82d2ca9a518d
fileciakk <filip.golak@gmail.com>
2017-12-18 23:33:15 +0100

    LiczbyPierwsze

---
+++ LiczbyPierwsze/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LiczbyPierwsze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbLiczbaWierszy = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generuj plik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLiczbaWierszy
+            // 
+            this.tbLiczbaWierszy.Location = new System.Drawing.Point(14, 43);
+            this.tbLiczbaWierszy.Name = "tbLiczbaWierszy";
+            this.tbLiczbaWierszy.Size = new System.Drawing.Size(99, 22);
+            this.tbLiczbaWierszy.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 93);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Pobierz Liczby Pierwsze";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(534, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Czyść";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 516);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Liczba wierszy";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tbLiczbaWierszy);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 91);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(326, 109);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(308, 516);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(247, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 91);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Wczytaj plik";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 631);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbLiczbaWierszy;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LiczbyPierwsze/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiczbyPierwsze
+{
+    public partial class Form1 : Form
+    {
+        //Program odczytujący plik tekstowy z liczbami całkowitymi
+        //i wyszukujący liczby pierwsze.
+        //Znalezione liczby mają być zapisane do pliku wynikowego.
+        //Plik źródłowy ma być najpierw wygenerowany losowo i
+        //zawierać przykładowo 1 miliard liczb.
+        //Program należy napisać w wersji wielowątkowej.
+        private int ID;
+        private Thread t;
+        public string sciezka = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                ThreadedWorker(i);
+            }
+        }
+
+        public void ThreadedWorker(int ID)
+        {
+            this.ID = ID;
+            t = new Thread(new ThreadStart(funkcjaGenerujPlik));
+            t.Start();
+        }
+
+        private void funkcjaGenerujPlik()
+        {
+            Random rnd = new Random();
+            string tekst = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                int liczba = rnd.Next(0, 10000);
+                tekst += liczba + "\r\n";
+                this.Invoke((MethodInvoker)(() => listBox1.Items.Add(liczba)));
+            }
+            //saveFileDialog1.Filter = "Text Files | *.txt";
+            //saveFileDialog1.ShowDialog();
+            //sciezka = saveFileDialog1.FileName;
+            //File.WriteAllText(sciezka, tekst);
+            //MessageBox.Show("Wygenerowano plik " + saveFileDialog1.FileName);
+            //tbLiczbaWierszy.Text = string.Empty;
+        }
+
+        static bool pierwsza(long a)
+        {
+            for (long j = 2; j <= (a / 2); j++)
+                if (a % j == 0)
+                    return false;
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tekst = "";
+            foreach (var row in listBox1.Items)
+            {
+                if (pierwsza(int.Parse(row.ToString())))
+                {
+                    listBox2.Items.Add(row.ToString());
+                    tekst += row.ToString() + "\r\n";
+                }
+            }
+            saveFileDialog2.Filter = "Text Files | *.txt";
+            saveFileDialog2.ShowDialog();
+            sciezka = saveFileDialog2.FileName;
+            File.WriteAllText(sciezka, tekst);
+            MessageBox.Show("Wygenerowano plik z liczbami Pierwszymi: " + sciezka);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            czysc();
+        }
+
+        private void czysc()
+        {
+            tbLiczbaWierszy.Text = string.Empty;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files | *.txt";
+            openFileDialog1.ShowDialog();
+            sciezka = openFileDialog1.FileName;
+
+            int counter = 0;
+            string line;
+            StreamReader file = new StreamReader(sciezka);
+            while ((line = file.ReadLine()) != null)
+            {
+                listBox1.Items.Add(line);
+            }
+            file.Close();
+        }
+    }
+}
